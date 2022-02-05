d264a4771eada0f18c10a1cfbc721409963c753a
Bilal Elmoussaoui <belmouss@redhat.com>
2022-02-05 12:44:55 +0100

    docs: Prepare for gi-docgen migration

---
+++ docs/urlmap.js
@@ -0,0 +1,5 @@
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ]
+]
